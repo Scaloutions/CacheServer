6ed03b7845f9bac4d32c8dca69dbd786ef81067f
Daria Sova <todariasova@gmail.com>
2018-04-15 11:19:09 -0700

    Add server skeleton
    Change-Id: I86ff9da035d2fe6772e8a8d98faa30c9be71d6bf

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func usage() {
+	fmt.Println("usage: example -logtostderr=true -stderrthreshold=[INFO|WARN|FATAL|ERROR] -log_dir=[string]\n")
+	flag.PrintDefaults()
+}
+
+func echoString(c *gin.Context) {
+	c.String(http.StatusOK, "Cache server is UP!")
+}
+
+func main() {
+	router := gin.Default()
+
+	//glog initialization flags
+	flag.Usage = usage
+	flag.Parse()
+
+	api := router.Group("/api")
+	{
+		api.GET("/test", echoString)
+		// api.GET("/get_quote", getQuoteReq)
+		api.POST("/quote", echoString)
+	}
+
+	log.Fatal(router.Run(":9092"))
+
+}
